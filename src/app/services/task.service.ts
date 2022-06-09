7fb4606ea04fa81d3525c81689a57fc6dca919c7
Emmanuel Adiba <eabaagah@gmail.com>
2022-06-09 13:19:38 +0000

    update db.json

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Task } from '../Task';
-import { TASKS } from '../mock-tasks';
-import { Observable, of } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl: string = "http://localhost:5000/tasks"
+
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS)
-    return tasks
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
